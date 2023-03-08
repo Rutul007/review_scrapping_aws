ff075638436c546325eb13f35dd1e9898c33109b
Rutul007 <93912541+Rutul007@users.noreply.github.com>
2023-03-08 20:42:31 +0530

    Update application.py

--- application.py
+++ application.py
@@ -3,8 +3,7 @@ from flask_cors import CORS,cross_origin
 import requests
 from bs4 import BeautifulSoup as bs
 from urllib.request import urlopen as uReq
-import logging
-logging.basicConfig(filename="scrapper.log" , level=logging.INFO)
+
 
 app = Flask(__name__)
 
